abffdfb71107c28201defe0f1a5f91fb414d1d6c
tincho <martin.salinas@gmail.com>
2021-06-19 17:15:50 -0300

    fix: forgot 1 rename

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
@@ -22,13 +23,27 @@ export default function DashboardPage() {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
+  const alarms = {
+    active: 4,
+    total: 10,
+  };
+  const { active, total } = alarms;
+
   return (
     <>
       <Grid item xs={12} md={6}>
-        <Paper className={fixedHeightPaper}>{"2 / 10 active alarms"}</Paper>
+        <Paper className={fixedHeightPaper}>
+          <Typography component="h4" variant="h4">
+            {active} / {total} active alarms
+          </Typography>
+        </Paper>
       </Grid>
       <Grid item xs={12} md={6}>
-        <Paper className={fixedHeightPaper}>{"To Be Implemented"}</Paper>
+        <Paper className={fixedHeightPaper}>
+          <Typography component="h4" variant="h4">
+            To Be Implemented
+          </Typography>
+        </Paper>
       </Grid>
     </>
   );
